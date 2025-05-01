53a3643da5a398d7edaa030683e94c07d94336ce
zhujingze <73783778+zhujingze@users.noreply.github.com>
2025-05-01 15:25:28 +0800

    Update tfqa.sh

--- tfqa.sh
+++ tfqa.sh
@@ -1,70 +1,52 @@
-(
-CUDA_VISIBLE_DEVICES=0 python /home/liang/project/concept/method/sled/run_tfqa.py \
-    --model-name /home/liang/project/mmlu/llama2 \
-    --data_path /home/liang/project/concept/benchmark/truqa/data/v0 \
-    --output-path 2-16_c.json \
-    --num-gpus 1 \
-    --decoding_method 'attn' \
-    --start_layer 2 \
-    --end_layer 16 \
-    --attn_alpha 1.5 \
-    --token_enhance None \
-    --token_weaken ac \
-    --alpha 1
-) &
+layer_pairs=(
+    # "4 20"
+    "5 20"
+    # "0 31" 
+)
 
-(
-CUDA_VISIBLE_DEVICES=2 python /home/liang/project/concept/method/sled/run_tfqa.py \
-    --model-name /home/liang/project/mmlu/llama2 \
-    --data_path /home/liang/project/concept/benchmark/truqa/data/v0 \
-    --output-path 2-9_p.json \
-    --num-gpus 1 \
-    --decoding_method 'attn' \
-    --start_layer 2 \
-    --end_layer 9 \
-    --attn_alpha 1.5 \
-    --token_enhance None \
-    --token_weaken p \
-    --alpha 1
-) &
+# 定义其他参数池
+ths=(0.05)       # 阈值候选值
+betas=(0)       # beta参数候选值
 
-(
-CUDA_VISIBLE_DEVICES=3 python /home/liang/project/concept/method/sled/run_tfqa.py \
-    --model-name /home/liang/project/mmlu/llama2 \
-    --data_path /home/liang/project/concept/benchmark/truqa/data/v0 \
-    --output-path 2-9_c.json \
-    --num-gpus 1 \
-    --decoding_method 'attn' \
-    --start_layer 2 \
-    --end_layer 9 \
-    --attn_alpha 1.5 \
-    --token_enhance None \
-    --token_weaken ac \
-    --alpha 1
-) &
+# 固定参数
+token_enhance="None"
+token_weaken="ac"
+num_gpus=1
+alpha=0.5
 
-(
-CUDA_VISIBLE_DEVICES=4 python /home/liang/project/concept/method/sled/run_tfqa.py \
-    --model-name /home/liang/project/mmlu/llama2 \
-    --data_path /home/liang/project/concept/benchmark/truqa/data/v0 \
-    --output-path 8-16_c.json \
-    --num-gpus 1 \
-    --decoding_method 'attn' \
-    --start_layer 8 \
-    --end_layer 16 \
-    --attn_alpha 1.5 \
-    --token_enhance None \
-    --token_weaken ac \
-    --alpha 1
-) &
-# (
-# CUDA_VISIBLE_DEVICES=0 python /home/liang/project/concept/method/sled/run_tfqa.py --model-name /home/liang/project/mmlu/llama2  --data_path /home/liang/project/concept/benchmark/truqa/data/v1 --output-path ori.json --num-gpus 1 --decoding_method VanillaGreedy > /home/liang/project/concept/method/sled/ori.txt
-# ) &
-# (
-# CUDA_VISIBLE_DEVICES=4 python /home/liang/project/concept/method/sled/run_tfqa.py --model-name /home/liang/project/mmlu/llama2  --data_path /home/liang/project/concept/benchmark/truqa/data/v1 --output-path dola.json --num-gpus 1 --decoding_method dola > /home/liang/project/concept/method/sled/dola.txt
-# ) &
-# (
-# CUDA_VISIBLE_DEVICES=5 python /home/liang/project/concept/method/sled/run_tfqa.py --model-name /home/liang/project/mmlu/llama2  --data_path /home/liang/project/concept/benchmark/truqa/data/v1 --output-path sled.json --num-gpus 1 --decoding_method SLED --evolution_rate 2.5  --evolution_scale 75 > /home/liang/project/concept/method/sled/sled.txt
-# ) &
+# 遍历所有参数组合
+for pair in "${layer_pairs[@]}"; do
+    # 解析层数组合
+    IFS=' ' read -r start_layer end_layer <<< "$pair"
+    
+    for th in "${ths[@]}"; do
+        for beta in "${betas[@]}"; do
+            # 动态生成输出文件名
+            output_file="/home/liang/project/concept/method/ConceptToken/tfqa_res/start${start_layer}_end${end_layer}_th${th}_beta${beta}.json"
+            
+            # 执行命令
+            CUDA_VISIBLE_DEVICES=4 python /home/liang/project/concept/method/ConceptToken/run_tfqa.py \
+                --model-name /home/liang/project/mmlu/llama2_13 \
+                --data_path /home/liang/project/mmlu/benchmark/truqa \
+                --output-path "$output_file" \
+                --num-gpus $num_gpus \
+                --decoding_method 'attn' \
+                --start_layer $start_layer \
+                --end_layer $end_layer \
+                --attn_alpha $alpha \
+                --token_enhance "$token_enhance" \
+                --token_weaken "$token_weaken" \
+                --th $th \
+                --beta $beta  &
+            
+            # 控制并行进程数（按需调整）
+            #wait_if_max_processes  # 需自定义进程控制函数
+            wait
+        done
+    done
+done
+
+# 等待所有任务完成
+wait
 
 wait
